9cf7a08c75667fc0157af0bd69380933d9db5d8b
2zemka <110781404+2zemka@users.noreply.github.com>
2023-05-30 17:02:55 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,4 @@
+valid_email = 'melkiibkru@gmail.com'
+valid_password = '12345'
+_auth_key = {"key": "038bea5f5cbdcd6b206b243be4601760c983cb2441de3ce641926add"}
+invalid_auth_key = {"key": "666bea5f5cbdcd6b206b243be4601760c983cb2441de3ce641926add"}
